39135070963e6035092da0b9a18722c3233f66fd
Nils Thomann <nils.thomann@quickline.ch>
2026-08-18 23:04:56 +0200

    test

--- src/BexioOrderImport.Tests/ProfileManagerServiceTests.cs
+++ src/BexioOrderImport.Tests/ProfileManagerServiceTests.cs
@@ -67,4 +67,46 @@ public class ProfileManagerServiceTests
         profiles.Should().HaveCount(1);
         profiles.Should().NotContain(customP);
     }
+
+    [Test]
+    public void CreateProfile_WhenUserCancels_ReturnsNull()
+    {
+        var profiles = new ObservableCollection<MappingProfile>
+        {
+            new MappingProfile { Name = "Default", Mapping = new ExcelMappingOptions() }
+        };
+
+        _dialogMock.Setup(d => d.ShowProfileCreateDialog(false)).Returns((string?)null);
+
+        var created = _service.CreateProfile(profiles);
+
+        created.Should().BeNull();
+        profiles.Should().HaveCount(1);
+    }
+
+    [Test]
+    public void CloneProfile_WhenUserCancels_ReturnsNull()
+    {
+        var defaultP = new MappingProfile { Name = "Default", Mapping = new ExcelMappingOptions() };
+        var profiles = new ObservableCollection<MappingProfile> { defaultP };
+
+        _dialogMock.Setup(d => d.ShowProfileCreateDialog(true)).Returns((string?)null);
+
+        var cloned = _service.CloneProfile(profiles, defaultP);
+
+        cloned.Should().BeNull();
+        profiles.Should().HaveCount(1);
+    }
+
+    [Test]
+    public void ImportProfiles_WhenFileReadThrowsException_ShowsErrorAndReturnsFalse()
+    {
+        var profiles = new ObservableCollection<MappingProfile>();
+        _dialogMock.Setup(d => d.ShowOpenFileDialog(It.IsAny<string>(), It.IsAny<string>())).Returns("invalid_file_path.json");
+
+        bool result = _service.ImportProfiles(profiles, s => { });
+
+        result.Should().BeFalse();
+        _dialogMock.Verify(d => d.ShowErrorDialog(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+    }
 }
